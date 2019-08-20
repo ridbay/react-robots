d3de80b19f839d7867406aaa5834e900a524e0e4
Artoas <artoas@Artoass-MacBook.Dual-Band-Broadband-Modem>
2019-08-20 12:35:41 +0100

    Updates

---
+++ precache-manifest.9301fdddbfba2a59f3147b2df0fcd850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4366d0b03be256a76fc4e92cb4320c2a",
+    "url": "/react-robots/index.html"
+  },
+  {
+    "revision": "6ea9cffab8331c659eaa",
+    "url": "/react-robots/static/css/main.029d304c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-robots/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6ea9cffab8331c659eaa",
+    "url": "/react-robots/static/js/main.228bb249.chunk.js"
+  },
+  {
+    "revision": "cf4c0b6cfcd07abc03ed",
+    "url": "/react-robots/static/js/runtime~main.d170d28d.js"
+  }
+]);
