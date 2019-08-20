6f5afc0809f7b5d80c013a6d2615c0a0fce385ce
Ridwan Balogun <balogunridwan@gmail.com>
2019-08-20 08:21:39 +0100

    change api

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,11 +3,12 @@ import './card.style.css'
 export const Card = (props)=> (
     <div className='card-container'>
         <img alt='monsters' src={`https://robohash.org/${props.monster.id}.png?set=set2&size=180x180`} />
-        <h2>{props.monster.name}</h2>
-        <p>{props.monster.email}</p>
-        <p>{props.monster.monstername}</p>
-        <p>{props.monster.phone}</p>
-        <p>{props.monster.website}</p>
+        <h2>Name: {props.monster.name}</h2>
+        <p>Email: {props.monster.email}</p>
+        <p>Username: {props.monster.username}</p>
+        <p>Phone: {props.monster.phone}</p>
+        <p>Address: {props.monster.address.street}</p>
+        <p>Website: {props.monster.website}</p>
     </div>
     
 )
